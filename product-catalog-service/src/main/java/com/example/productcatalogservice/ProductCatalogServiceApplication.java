8cb22bba2f375848b02b1eae14c608089441b98a
Cesar <cdquintero-2022a@corhuila.edu.co>
2024-09-11 15:54:34 -0500

    Proyecto inicial

--- product-catalog-service/src/main/java/com/example/productcatalogservice/ProductCatalogServiceApplication.java
+++ product-catalog-service/src/main/java/com/example/productcatalogservice/ProductCatalogServiceApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProductCatalogServiceApplication {
 
+
+    //TODO
     public static void main(String[] args) {
         SpringApplication.run(ProductCatalogServiceApplication.class, args);
     }
